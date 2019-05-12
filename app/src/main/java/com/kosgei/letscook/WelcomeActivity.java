10e49abcfe138ced0a25d7edf243ec9bdd4c63f4
iamkosgei <kosgeik2930@gmail.com>
2019-05-12 09:49:32 +0300

    added an onclick listener on buttons to navigate to different screens.

--- app/src/main/java/com/kosgei/letscook/WelcomeActivity.java
+++ app/src/main/java/com/kosgei/letscook/WelcomeActivity.java
@@ -2,15 +2,17 @@ package com.kosgei.letscook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.to_login) Button toLogin;
     @BindView(R.id.to_signup) Button toSignUp;
     @BindView(R.id.title) TextView title;
@@ -24,5 +26,22 @@ public class WelcomeActivity extends AppCompatActivity {
 
         Typeface typeface = Typeface.createFromAsset(getAssets(),"fonts/Hollaboi FREE.ttf");
         title.setTypeface(typeface);
+
+        toLogin.setOnClickListener(this);
+        toSignUp.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == toLogin)
+        {
+            startActivity(new Intent(WelcomeActivity.this,LoginActivity.class));
+        }
+
+        else if (v == toSignUp)
+        {
+            startActivity(new Intent(WelcomeActivity.this,SignUpActivity.class));
+        }
+
     }
 }
