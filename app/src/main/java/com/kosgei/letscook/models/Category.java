2bf96f341e2d4d508152893a91c894c2c1c4450a
iamkosgei <kosgeik2930@gmail.com>
2019-05-12 20:47:42 +0300

    created a Catogory model to be used to create category objects.

---
+++ app/src/main/java/com/kosgei/letscook/models/Category.java
@@ -0,0 +1,20 @@
+package com.kosgei.letscook.models;
+
+public class Category {
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
