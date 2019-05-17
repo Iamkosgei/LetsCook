94b89c09ac40e258ad9c056d213185f238a0d501
iamkosgei <kosgeik2930@gmail.com>
2019-05-17 16:58:03 +0300

    added ability for users to signup

--- app/src/main/java/com/kosgei/letscook/SignUpActivity.java
+++ app/src/main/java/com/kosgei/letscook/SignUpActivity.java
@@ -1,13 +1,25 @@
 package com.kosgei.letscook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.wang.avi.AVLoadingIndicatorView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,6 +31,13 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     @BindView(R.id.confirm_password) EditText confirm_password;
     @BindView(R.id.sign_up) Button signUp;
 
+    @BindView(R.id.loading)
+    AVLoadingIndicatorView loadingIndicatorView;
+
+
+    private FirebaseAuth mAuth;
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +46,15 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
         ButterKnife.bind(this);
 
+        loadingIndicatorView.hide();
+
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
         signUp.setOnClickListener(this);
-
-        if (isValid())
-        {
-
-        }
 
     }
 
@@ -60,7 +82,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         {
             confirm_password.setError("Please enter a password");
         }
-        else if(!(confirm_password.getText().toString().trim().equals(password.getText().toString())))
+        else if(!(confirm_password.getText().toString().trim().equals(password.getText().toString().trim())))
         {
             confirm_password.setError("Password doesn't match");
         }
@@ -78,7 +100,33 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         {
             if(isValid())
             {
-                Toast.makeText(this, names.getText() + " welcome", Toast.LENGTH_SHORT).show();
+                loadingIndicatorView.show();
+                signUp.setEnabled(false);
+                mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    loadingIndicatorView.hide();
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    mDatabase.child("Users").child(user.getUid()).child("name").setValue(names.getText().toString()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Intent intent = new Intent(SignUpActivity.this,HomeActivity.class);
+
+                                            //avoids using the back btn to go back to the signup activity after successful login
+                                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                            startActivity(intent);
+                                        }
+                                    });
+                                } else {
+                                    loadingIndicatorView.hide();
+                                    Toast.makeText(SignUpActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    signUp.setEnabled(true);
+                                }
+                            }
+                        });
             }
 
         }
