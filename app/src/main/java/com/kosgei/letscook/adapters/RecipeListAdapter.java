79bce47226997e5765af59eaf91aeb65ba648e14
iamkosgei <kosgeik2930@gmail.com>
2019-05-19 15:27:37 +0300

    adapter to be used to display a list of recipes

---
+++ app/src/main/java/com/kosgei/letscook/adapters/RecipeListAdapter.java
@@ -0,0 +1,78 @@
+package com.kosgei.letscook.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.kosgei.letscook.R;
+import com.kosgei.letscook.models.Recipe;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.RecipeViewHolder> {
+    private Context context;
+    private ArrayList<Recipe> recipes;
+
+    public RecipeListAdapter(Context context,ArrayList<Recipe> recipes) {
+        this.context = context;
+        this.recipes = recipes;
+    }
+
+    @NonNull
+    @Override
+    public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_list_item,parent,false);
+        return new RecipeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecipeViewHolder holder, int position) {
+        holder.bindRecipe(recipes.get(position));
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return recipes.size();
+    }
+
+    public class RecipeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        @BindView(R.id.recipe_name)
+        TextView recipeTV;
+        @BindView(R.id.recipe_image)
+        ImageView recipeImageView;
+
+        private Context context;
+
+        RecipeViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            ButterKnife.bind(this,itemView);
+            context = itemView.getContext();
+            itemView.setOnClickListener(this);
+        }
+
+       public void bindRecipe(Recipe recipe)
+        {
+            recipeTV.setText(recipe.getName());
+            Picasso.get().load(recipe.getImage()).into(recipeImageView);
+        }
+
+
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+}
