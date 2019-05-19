8b9b62fa1f6775b3a069082fc5fcd2c7f2f41242
iamkosgei <kosgeik2930@gmail.com>
2019-05-19 15:30:42 +0300

    added the recipe list adapter

--- app/src/main/java/com/kosgei/letscook/ui/RecipeListActivity.java
+++ app/src/main/java/com/kosgei/letscook/ui/RecipeListActivity.java
@@ -1,29 +1,50 @@
 package com.kosgei.letscook.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.kosgei.letscook.R;
+import com.kosgei.letscook.adapters.RecipeListAdapter;
+import com.kosgei.letscook.models.Recipe;
 import com.kosgei.letscook.services.EdamamService;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 
 public class RecipeListActivity extends AppCompatActivity {
-    String category = "chicken";
+
     private static final String TAG = "RecipeListActivity";
+    @BindView(R.id.recipeListRecycler)
+    RecyclerView recyclerView;
+
+    RecipeListAdapter recipeListAdapter;
+
+    ArrayList<Recipe> recipes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
 
+        ButterKnife.bind(this);
+
+        //get category from home activity
+        Intent intent = getIntent();
+        String category = intent.getStringExtra("category");
         getRecipes(category);
+
     }
 
     private void getRecipes(String category)
@@ -38,13 +59,20 @@ public class RecipeListActivity extends AppCompatActivity {
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                try {
-                    String jsonData = response.body().string();
-                    Log.v(TAG, jsonData);
+                recipes = edamamService.processResults(response);
 
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                RecipeListActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        recipeListAdapter = new RecipeListAdapter(getApplicationContext(),recipes);
+                        recyclerView.setAdapter(recipeListAdapter);
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+                        recyclerView.setLayoutManager(layoutManager);
+                        recyclerView.setHasFixedSize(true);
+
+                    }
+                });
+
             }
         });
     }
