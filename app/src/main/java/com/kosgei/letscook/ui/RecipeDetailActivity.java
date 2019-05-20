0d4a9eaf2fa619f1a4fc8264fd81975ae442cef8
iamkosgei <kosgeik2930@gmail.com>
2019-05-20 11:24:00 +0300

    recipe detail activity to hold the fragment that will display details about a given recipe

---
+++ app/src/main/java/com/kosgei/letscook/ui/RecipeDetailActivity.java
@@ -0,0 +1,37 @@
+package com.kosgei.letscook.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.kosgei.letscook.R;
+import com.kosgei.letscook.adapters.RecipePagerAdapter;
+import com.kosgei.letscook.models.Recipe;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RecipeDetailActivity extends AppCompatActivity {
+    @BindView(R.id.viewPager)
+    ViewPager viewPager;
+    private RecipePagerAdapter recipePagerAdapter;
+    ArrayList<Recipe> recipes = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_detail);
+
+        ButterKnife.bind(this);
+
+        recipes = Parcels.unwrap(getIntent().getParcelableExtra("recipes"));
+        int startingPosition = getIntent().getIntExtra("position",0);
+        recipePagerAdapter = new RecipePagerAdapter(getSupportFragmentManager(),recipes);
+        viewPager.setAdapter(recipePagerAdapter);
+        viewPager.setCurrentItem(startingPosition);
+    }
+}
