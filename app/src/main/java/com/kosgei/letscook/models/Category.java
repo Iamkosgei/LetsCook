b263fa901029964e6905384df623ba34733f32a4
iamkosgei <kosgeik2930@gmail.com>
2019-05-27 11:22:40 +0300

    added more fields

--- app/src/main/java/com/kosgei/letscook/models/Category.java
+++ app/src/main/java/com/kosgei/letscook/models/Category.java
@@ -1,15 +1,27 @@
 package com.kosgei.letscook.models;
 
+import org.parceler.Parcel;
+
+@Parcel
 public class Category {
-    private String name;
-    private String url;
+    private String id,name,url,category;
 
     public Category() {
     }
 
-    public Category(String name, String url) {
+    public Category(String id, String name, String url, String category) {
+        this.id = id;
         this.name = name;
         this.url = url;
+        this.category = category;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -26,5 +38,13 @@ public class Category {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 }
