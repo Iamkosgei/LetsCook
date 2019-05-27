cea61660dc85e384409464a80ad8d628b4715323
iamkosgei <kosgeik2930@gmail.com>
2019-05-27 11:37:23 +0300

    added more end points

--- app/src/main/java/com/kosgei/letscook/Constants.java
+++ app/src/main/java/com/kosgei/letscook/Constants.java
@@ -10,5 +10,12 @@ public class Constants {
     public static final String EDAMAM_TO_QUERY_PARAMETER = "to";
     public static final String FIREBASE_CHILD_RECIPES = "recipes";
 
+    public static final String THE_MEAL_DB_LATEST_URL = "https://www.themealdb.com/api/json/v1/1/latest.php";
+    public static final String THE_MEAL_DB_ALL_CATEGORIES_URL = "https://www.themealdb.com/api/json/v1/1/categories.php";
+    public static final String THE_MEAL_DB_FILTER_BY_CATEGORY = "https://www.themealdb.com/api/json/v1/1/filter.php";
+
+    public static final String THE_MEAL_DB_FIND_MEAL_BY_ID = "https://www.themealdb.com/api/json/v1/1/lookup.php";
+
+
 
 }
