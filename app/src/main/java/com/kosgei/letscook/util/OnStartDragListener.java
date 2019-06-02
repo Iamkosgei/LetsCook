411872c049285a3168611455247c7c5ddb9183d1
iamkosgei <kosgeik2930@gmail.com>
2019-06-02 11:20:56 +0300

    added this interface to handle drag-and-drop interaction with the touchscreen

---
+++ app/src/main/java/com/kosgei/letscook/util/OnStartDragListener.java
@@ -0,0 +1,7 @@
+package com.kosgei.letscook.util;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+}
