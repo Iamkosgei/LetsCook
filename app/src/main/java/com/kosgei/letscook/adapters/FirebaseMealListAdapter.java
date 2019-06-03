f5e858dd80e332766dda56c5bb978ee93c66c38d
iamkosgei <kosgeik2930@gmail.com>
2019-06-03 12:00:46 +0300

    extending the recycler adapter in order to add more functionality to it.

---
+++ app/src/main/java/com/kosgei/letscook/adapters/FirebaseMealListAdapter.java
@@ -0,0 +1,53 @@
+package com.kosgei.letscook.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.kosgei.letscook.R;
+import com.kosgei.letscook.models.Meal;
+import com.kosgei.letscook.util.ItemTouchHelperAdapter;
+import com.kosgei.letscook.util.OnStartDragListener;
+
+public class FirebaseMealListAdapter extends FirebaseRecyclerAdapter<Meal,FirebaseRecipeViewHolder> implements ItemTouchHelperAdapter {
+    private DatabaseReference mRef;
+    private OnStartDragListener mOnStartDragListener;
+    private Context mContext;
+
+    public FirebaseMealListAdapter(FirebaseRecyclerOptions<Meal> options,
+                                   DatabaseReference ref,
+                                   OnStartDragListener onStartDragListener,
+                                   Context context){
+        super(options);
+        mRef = ref.getRef();
+        mOnStartDragListener = onStartDragListener;
+        mContext = context;
+    }
+    @Override
+    protected void onBindViewHolder(@NonNull FirebaseRecipeViewHolder firebaseRecipeViewHolder, int position, @NonNull Meal meal) {
+        firebaseRecipeViewHolder.bindRestaurant(meal);
+    }
+
+    @NonNull
+    @Override
+    public FirebaseRecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_list_item_2, parent, false);
+        return new FirebaseRecipeViewHolder (view);
+    }
+
+    @Override
+    public boolean onItemMove(int fromPosition, int toPosition){
+        return false;
+    }
+
+    @Override
+    public void onItemDismiss(int position){
+
+    }
+}
