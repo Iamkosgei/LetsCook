b2c33729416b392efbc413bc8c8f61bcc304f1a9
iamkosgei <kosgeik2930@gmail.com>
2019-06-06 05:57:34 +0300

    added a check for orientation inorder for it to display the correct layout based on the orientation.

--- app/src/main/java/com/kosgei/letscook/adapters/RecipeListAdapter.java
+++ app/src/main/java/com/kosgei/letscook/adapters/RecipeListAdapter.java
@@ -2,6 +2,7 @@ package com.kosgei.letscook.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,12 +10,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.kosgei.letscook.R;
 import com.kosgei.letscook.models.Meal;
 import com.kosgei.letscook.models.Recipe;
 import com.kosgei.letscook.ui.RecipeDetailActivity;
+import com.kosgei.letscook.ui.RecipeDetailFragment;
 import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
@@ -36,7 +40,7 @@ public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.Re
     @NonNull
     @Override
     public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_list_item_2,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_list_item_3,parent,false);
         return new RecipeViewHolder(view);
     }
 
@@ -59,12 +63,34 @@ public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.Re
 
         private Context context;
 
+        private int mOrientation;
+
         RecipeViewHolder(@NonNull View itemView) {
             super(itemView);
 
             ButterKnife.bind(this,itemView);
             context = itemView.getContext();
             itemView.setOnClickListener(this);
+
+            mOrientation = itemView.getResources().getConfiguration().orientation;
+
+            // Checks if the recorded orientation matches Android's landscape configuration.
+            // if so, we create a new DetailFragment to display in our special landscape layout:
+            if (mOrientation == Configuration.ORIENTATION_LANDSCAPE) {
+                createDetailFragment(0);
+            }
+        }
+
+        // Takes position of restaurant in list as parameter:
+        private void createDetailFragment(int position) {
+            // Creates new RestaurantDetailFragment with the given position:
+            RecipeDetailFragment detailFragment = RecipeDetailFragment.newInstance(recipes, position);
+            // Gathers necessary components to replace the FrameLayout in the layout with the RestaurantDetailFragment:
+            FragmentTransaction ft = ((FragmentActivity) context).getSupportFragmentManager().beginTransaction();
+            //  Replaces the FrameLayout with the RestaurantDetailFragment:
+            ft.replace(R.id.recipeDetailContainer, detailFragment);
+            // Commits these changes:
+            ft.commit();
         }
 
        public void bindRecipe(Meal meal)
@@ -78,11 +104,16 @@ public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.Re
         @Override
         public void onClick(View v) {
             int itemPosition = getLayoutPosition();
-            Intent intent = new Intent(context, RecipeDetailActivity.class);
-            intent.putExtra("position", itemPosition);
-            intent.putExtra("recipes", Parcels.wrap(recipes));
-            context.startActivity(intent);
 
+            if (mOrientation == Configuration.ORIENTATION_LANDSCAPE) {
+                createDetailFragment(itemPosition);
+            } else
+            {
+                Intent intent = new Intent(context, RecipeDetailActivity.class);
+                intent.putExtra("position", itemPosition);
+                intent.putExtra("recipes", Parcels.wrap(recipes));
+                context.startActivity(intent);
+            }
         }
     }
 }
