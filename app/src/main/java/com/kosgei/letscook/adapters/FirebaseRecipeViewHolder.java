a5e63a98f1113863d2e79ac1d3023cfeb4a41f45
iamkosgei <kosgeik2930@gmail.com>
2019-06-06 05:53:22 +0300

    implemented the ItemTouchHelperViewHolder inorder to animate various gestures

--- app/src/main/java/com/kosgei/letscook/adapters/FirebaseRecipeViewHolder.java
+++ app/src/main/java/com/kosgei/letscook/adapters/FirebaseRecipeViewHolder.java
@@ -22,59 +22,78 @@ import com.kosgei.letscook.R;
 import com.kosgei.letscook.models.Meal;
 import com.kosgei.letscook.models.Recipe;
 import com.kosgei.letscook.ui.RecipeDetailActivity;
+import com.kosgei.letscook.util.ItemTouchHelperViewHolder;
 import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
-public class FirebaseRecipeViewHolder  extends RecyclerView.ViewHolder implements View.OnClickListener {
+public class FirebaseRecipeViewHolder  extends RecyclerView.ViewHolder implements ItemTouchHelperViewHolder {
 
     View mView;
     Context mContext;
     FirebaseUser user;
+    public ImageView image;
 
     public FirebaseRecipeViewHolder(View itemView) {
         super(itemView);
         mView = itemView;
 
         mContext = itemView.getContext();
-        itemView.setOnClickListener(this);
+//        itemView.setOnClickListener(this);
         user = FirebaseAuth.getInstance().getCurrentUser();
     }
 
     public void bindRestaurant(Meal recipe) {
         TextView name = mView.findViewById(R.id.recipe_name);
-        ImageView image = mView.findViewById(R.id.recipe_image);
+        image = mView.findViewById(R.id.recipe_image);
 
         Picasso.get().load(recipe.getImage()).into(image);
         name.setText(recipe.getName());
     }
 
     @Override
-    public void onClick(View view) {
-        final ArrayList<Meal> recipes = new ArrayList<>();
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RECIPES).child(user.getUid());
-
-        ref.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    recipes.add(snapshot.getValue(Meal.class));
-                }
-
-                int itemPosition = getLayoutPosition();
-
-                Intent intent = new Intent(mContext, RecipeDetailActivity.class);
-                intent.putExtra("position", itemPosition + "");
-                intent.putExtra("recipes", Parcels.wrap(recipes));
-
-                mContext.startActivity(intent);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-            }
-        });
+    public void onItemSelected() {
+        itemView.animate()
+                .alpha(0.7f)
+                .scaleX(0.9f)
+                .scaleY(0.9f)
+                .setDuration(500);
     }
+
+    @Override
+    public void onItemClear() {
+        itemView.animate()
+                .alpha(1f)
+                .scaleX(1f)
+                .scaleY(1f);
+    }
+
+//    @Override
+//    public void onClick(View view) {
+//        final ArrayList<Meal> recipes = new ArrayList<>();
+//        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RECIPES).child(user.getUid());
+//
+//        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+//                    recipes.add(snapshot.getValue(Meal.class));
+//                }
+//
+//                int itemPosition = getLayoutPosition();
+//
+//                Intent intent = new Intent(mContext, RecipeDetailActivity.class);
+//                intent.putExtra("position", itemPosition + "");
+//                intent.putExtra("recipes", Parcels.wrap(recipes));
+//
+//                mContext.startActivity(intent);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//            }
+//        });
+//    }
 }
